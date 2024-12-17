dae5aad68b04da2fff73b80876d8d0c52f9b0484
Andriniaina RAHERISON <a.raherison@etech.local>
2024-12-17 15:15:59 +0300

    Added basic node graph

---
+++ src/views/components/Graph.tsx
@@ -0,0 +1,53 @@
+import { ReactFlow, Handle, Position, MarkerType } from "@xyflow/react";
+
+import "@xyflow/react/dist/style.css";
+
+const nodeTypes = {
+  custom: CustomNode,
+  final: FinalNode
+};
+
+const initialNodes = [
+  { id: "d", position: { x: 0, y: 0 }, data: { label: "1", section: "AC" }, type: "custom" },
+  { id: "abc", position: { x: 0, y: 100 }, data: { label: "2", section: "AC" }, type: "final" },
+];
+const initialEdges = [{ id: "e1-2", source: "d", target: "abc", reconnectable: true, markerEnd: { type: MarkerType.ArrowClosed } }];
+
+function Graph() {
+  return (
+    <div className="w-[500px] h-[300px] border-solid border-red-300 border-2 rounded-md">
+      <ReactFlow
+        nodes={initialNodes}
+        edges={initialEdges}
+        nodeTypes={nodeTypes}
+        fitView={true}
+      />
+    </div>
+  );
+}
+
+function CustomNode({ data }: any) {
+  return (
+    <>
+      <div className="bg-blue-950 rounded-full w-14 h-14 flex flex-col items-center justify-center">
+      <div className="text-center text-white">{data.section}</div>
+        <div className="text-center text-white">{data.label}</div>
+      </div>
+      <Handle type="source" position={Position.Bottom} style={{visibility: "hidden", bottom: "3px"}} />
+    </>
+  );
+}
+
+function FinalNode({ data }: any) {
+  return (
+    <>
+      <Handle type="target" position={Position.Top} style={{visibility: "hidden", top: "3px"}} />
+      <div className="bg-red-600 rounded-full w-14 h-14 flex flex-col items-center justify-center p-2">
+      <div className="text-center text-white">{data.section}</div>
+        <div className="text-center text-white">{data.label}</div>
+      </div>
+    </>
+  );
+}
+
+export default Graph;
--- src/views/pages/Result.tsx
+++ src/views/pages/Result.tsx
@@ -1,38 +1,52 @@
-import Logo from "../../assets/images/Logo.png"
-import Title from "../../assets/images/Title.png"
+import Logo from "../../assets/images/Logo.png";
+import Title from "../../assets/images/Title.png";
+import Graph from "../components/Graph";
 
 function Result() {
   return (
-    <div className='flex flex-col'>
-        <div className="flex items-center justify-center py-16 border-b-4 border-solid border-black">
-            <img src={Title} alt="title" />
+    <div className="flex flex-col">
+      <div className="flex items-center justify-center py-16 border-b-4 border-solid border-black">
+        <img src={Title} alt="title" />
+      </div>
+      <div className="flex flex-col px-20">
+        <span className="flex justify-center font-bold text-xl pt-3">
+          CONCORDANCE CADASTRALE
+        </span>
+        <div className="flex flex-col pb-7 pt-10 text-bl font-medium">
+          <span>Référence :</span>
+          <span>Adresse du bien : 31 rue des bleuets – 72700 ROUILLON</span>
+          <span>Parcelle cadastrale : AC n°162</span>
         </div>
-        <div className="flex flex-col px-20">
-            <span className="flex justify-center font-bold text-xl pt-3">CONCORDANCE CADASTRALE</span>
-            <div className="flex flex-col pb-7 pt-10 text-bl font-medium">
-                <span>Référence :</span>
-                <span>Adresse du bien : 31 rue des bleuets – 72700 ROUILLON</span>
-                <span>Parcelle cadastrale : AC n°162</span>
-            </div>
-            <div className="flex flex-col gap-3 mb-5">
-                <span className="font-medium underline">Graphique de généalogie de la parcelle :</span>
-                <div className="w-[500px] h-[300px] border-solid border-red-300 border-2 rounded-md"></div>
-            </div>
-            <div className="flex flex-col gap-3">
-                <span className="font-medium underline">Tableau de généalogie de la parcelle AT 862 :</span>
-            </div>
+        <div className="flex flex-col gap-3 mb-5">
+          <span className="font-medium underline">
+            Graphique de généalogie de la parcelle :
+          </span>
+          <Graph />
         </div>
-        <div className='flex flex-row px-5'>
-            <div><img src={Logo} alt="logo" /></div>
-            <div className='flex flex-col px-5'>
-                <span>URBATECH SOLUTIONS</span>
-                <span>229 rue Saint-Honoré 75001 PARIS</span>
-                <span>SAS au capital de 100 000 euros, immatriculée au RCS de Paris sous le numéro 928 165 380</span>
-                <span>SIRET 92816538000017 - n° de TVA Intracommunautaire FR87928165380</span>
-            </div>
+        <div className="flex flex-col gap-3">
+          <span className="font-medium underline">
+            Tableau de généalogie de la parcelle AT 862 :
+          </span>
         </div>
+      </div>
+      <div className="flex flex-row px-5">
+        <div>
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="flex flex-col px-5">
+          <span>URBATECH SOLUTIONS</span>
+          <span>229 rue Saint-Honoré 75001 PARIS</span>
+          <span>
+            SAS au capital de 100 000 euros, immatriculée au RCS de Paris sous
+            le numéro 928 165 380
+          </span>
+          <span>
+            SIRET 92816538000017 - n° de TVA Intracommunautaire FR87928165380
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Result
+export default Result;
